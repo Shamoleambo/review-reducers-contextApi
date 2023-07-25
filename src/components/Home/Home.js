16a6eacb268db2586dc1dda7c303ee6a92181314
Tiago Gomes <tidgomes@gmail.com>
2023-07-25 15:13:59 -0300

    refactor logging out from the navigation component to use context

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,11 +4,11 @@ import Card from '../UI/Card/Card'
 import classes from './Home.module.css'
 import Button from '../UI/Button/Button'
 
-const Home = (props) => {
+const Home = ({ onLogout }) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button>
+      <Button onClick={onLogout}>Logout</Button>
     </Card>
   )
 }

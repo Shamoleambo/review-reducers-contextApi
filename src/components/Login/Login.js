6c136219af6f775fade570dcc7069df42fc9ef47
Tiago Gomes <tidgomes@gmail.com>
2023-07-25 13:01:32 -0300

    use cleanup function to clean the timer before next run

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false)
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    )
+    const timerIdentifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      )
+    }, 500)
+
+    return () => {
+      clearTimeout(timerIdentifier)
+    }
   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
